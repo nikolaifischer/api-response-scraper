c7096fe8fa12b9975ef8a576da82d7f455c513c5
nikolaifischer <nfischer@hm.edu>
2016-11-21 22:14:07 +0100

    Added removing of old documents
     - Everything older than 3 Months is removed. This is checked every day.
    - Renamed functions

--- app/scraper.js
+++ app/scraper.js
@@ -1,3 +1,9 @@
+/*
+	Scrapes the DB BahnPark API and saves the responses to a database.
+	API Responses older that 3 Months get deleted.
+	This is QUICK AND DIRTY. Deal with it.
+*/
+
 var http = require('http');
 var mongoose = require('mongoose');
 var config = require('../config');
@@ -24,10 +30,9 @@ mongoose.connection.on('connected', function () {
 
 
 
-	stealDatData = function() {
+	stealThatData = function() {
 
 		console.log("Stealing Data. Don't tell anyone.");
-
 
 		http.get({
 		  hostname: 'opendata.dbbahnpark.info',
@@ -64,7 +69,47 @@ mongoose.connection.on('connected', function () {
 		
 	}
 
-	setInterval(stealDatData, 60000*60); //every 60 mins
+	deleteThatData = function() {
+		console.log("Deleting old Data!");
+		parking.find({}, function(err, documents) {
+
+			if (err) {
+				console.log(err);
+				return;
+			}
+			if(documents.length>0) {
+				for(var i=0; i<documents.length; i++) {
+
+					var timeS= documents[i].entry.allocation.timestamp;
+					var data_time = new Date(timeS);
+					var now_time = new Date();
+					
+
+					if(now_time - data_time > 6.048e+8) { 
+						  //14 DAYS: 1.21e+9 3 DAYS: 2.592e+8, 3 MONTHS: 7.884e+9
+						parking.findOne({"_id": documents[i]._id}, function (err, removeThis) {
+							removeThis.remove(function (err){
+
+							if(err) {
+								console.log(err);
+							}
+							
+							});
+						});
+
+						
+					}
+
+				}
+
+			}
+		});
+	}
+
+	setInterval(stealThatData, 60000*60); //every 60 mins
+	setInterval(deleteThatData, 60000*60*24); //every Day
 
 });
+
+
 
